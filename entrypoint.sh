32c0ad0530f6d59ca5ed3e002e786c48fbd4d3bd
Owain Perry <operry@quantifisolutions.com>
2026-05-28 10:38:31 +0100

    updated path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p ~/.claude
 cp -r /claude/* /home/user/.claude  2>/dev/null
-cp /home/user/.claude/.claude.json /home/user/
+cp /claude/.claude.json /home/user/
 /home/user/.local/bin/claude
