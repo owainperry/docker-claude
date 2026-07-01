e4f6fe154ed22360d25ab536b22304f84948c317
Owain Perry <operry@quantifisolutions.com>
2026-07-01 09:41:25 +0100

    trying a different startup to use a more static .claude folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p ~/.claude
-cp -r /claude/* /home/user/.claude  2>/dev/null
-cp /claude/.claude.json /home/user/
-cp /claude/.credentials.json /home/user/
+cp -r /claude /home/user/.claude  2>/dev/null
+#cp /claude/.claude.json /home/user/
+#cp /claude/.credentials.json /home/user/
 /home/user/.local/bin/claude
